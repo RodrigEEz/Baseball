4b2890f6dfaaa825d77ae7b56f32a1b132a5a576
sebastian <rodriguezsebastian621@gmail.com>
2024-02-19 21:30:35 -0300

    Added connectivity to database

--- dependencies.py
+++ dependencies.py
@@ -1,39 +1,84 @@
-import json
+import sqlite3
+import time
 
 class Team:
-    """Defines team and it's resources during game"""
+    """A sample Team class"""
 
     def __init__(self):
-        self.selected_team  = self.select_team()
+        self.id, self.name, self.manager, self.players, self.pitcher = self.load_team()
         self.score = 0
-        self.batting_order = self.define_batting_order()
-        self.positions = self.define_positions()
 
-    def define_batting_order(self):
-        pass
+    def load_team(self):
+        """Looks for team information in the database"""
 
-    def define_positions(self):
+        select = 'SELECT * FROM TEAMS WHERE ID = ?'
 
-        positions = dict()
+        team_values = ()
 
-        for player in self.selected_team.keys():
-            positions[self.selected_team[player]['position']] = self.selected_team[player]['name']
+        con = sqlite3.connect('database.sqlite')
+        cur = con.cursor()
+        while team_values == ():
+            team = input('please select the team you want to play with: ')
+            selected = cur.execute(select, team)
+            try:
+                team_values = selected.fetchall()[0]
+                try:
+                    players, pitcher = self.load_players(team)
+                except IndexError:
+                    print('The team does not have players assigned. Please select other team.')
+                    team_values = ()
+            except IndexError:
+                print('Select a valid team.')
+                team_values = ()
 
-        return positions
+        name = team_values[1]
+        manager = team_values[2]
 
-    def select_team(self):
-        team = int(input('please select the team you want to play with: '))
+        return team, name, manager, players, pitcher
 
-        with open('data.json') as json_file:
-            data = json.load(json_file)
+    def load_players(self, team):
+        "Looks for players information in the database"
 
-        team = data['teams'][team]
+        players = list()
 
-        return team
+        select = "\
+                SELECT\
+                    A.ID,\
+                    A.NAME,\
+                    B.POSITION_NAME,\
+                    A.GAMES_PLAYED,\
+                    A.GAMES_WON,\
+                    A.GAMES_LOST\
+                FROM\
+	                PLAYERS A\
+                INNER JOIN\
+	                POSITIONS B\
+	                ON A.POSITION_ID = B.ID\
+                WHERE\
+                    A.TEAM_ID = ?"
+        
+        con = sqlite3.connect('database.sqlite')
+        cur = con.cursor()
+        selected = cur.execute(select, team)
+        players_values = selected.fetchall()
+
+        if players_values == []:
+            raise IndexError('Team without players')
+        else:
+            for player in players_values:
+                if player[2] == 'PITCHER':
+                    player_object = Pitcher(player[0], player[1], player[2], player[3:])  
+                    pitcher = player_object
+                else:
+                    player_object = Player(player[0], player[1], player[2], player[3:])
+                players.append(player_object)
+            
+            return players, pitcher
+    
         
     def rotate_batting_order(self, K=1):
         """When a batter hits the ball, he gets in the diamond, so the next one in the lineup comes to the bat"""
-        self.batting_order = self.batting_order[slice(K, len(self.batting_order))] + self.batting_order[slice(0, K)]
+        self.players = self.players[slice(K, len(self.players))] + self.players[slice(0, K)]
 
     def run(self, diamond, length ):
 
@@ -50,31 +95,80 @@ class Team:
         self.rotate_batting_order()
 
 
+class Player:
+    """A sample player class"""
+
+    def __init__(self , id, name, position, stats):
+        self.id  = id
+        self.name = name
+        self.position = position
+        self.stats = self.set_stats(stats, ['games_played', 'games_won', 'games_lost'])
+
+    def set_stats(self, stats, stats_names):
+        """set stats as a dictionary"""
+
+        stats_dict = dict()
+
+        i = 0
+
+        for stat in stats_names:
+            stats_dict[stat] = stats[i]
+            i +=1
+
+        return stats_dict
+
+class Pitcher(Player):
+    """A class for the pitcher"""
+
+    def __init__(self, id, name, position, stats):
+        super().__init__(id, name, position, stats)
+        pitching_stats = self.set_pitching_stats()
+
+    def set_pitching_stats(self):
+        """returns pitching stats"""
+
+        stats_names = ['accuracy']
+
+        select = 'SELECT\
+                        *\
+                  FROM\
+                        PITCHER_STATS\
+                  WHERE\
+                        PLAYER_ID = ?'
+
+        con = sqlite3.connect('database.sqlite')
+        cur = con.cursor()
+        selected = cur.execute(select, (str(self.id),))
+        stats = selected.fetchall()[0]
+
+        stats_dict = dict()
+
+        stats_dict = self.set_stats(stats, stats_names)
+
+        return stats_dict
+        
+
 class Field:
-    
+    """A sample Field class"""
 
     def __init__(self):
-        self.pitcher = ''
-        self.first_base = ''
-        self.second_base = ''
-        self.shortstop = ''
-        self.third_base = ''
-        self.catcher = ''
-        self.left_yard = ''
-        self.center_yard = ''
-        self.right_yard = ''
-        self.field = [0,0,0,0]
-
-    def fill_positions(self, team):
-        self.pitcher = team.positions['pitcher']
-        self.first_base = team.positions['first_base']
-        self.second_base = team.positions['second_base']
-        self.short_stop = team.positions['short_stop']
-        self.third_base = team.positions['third_base']
-        self.catcher = team.positions['catcher']
-        self.left_yard = team.positions['left_yard']
-        self.center_yard = team.positions['center_yard']
-        self.right_yard = team.positions['right_yard']
+        self.bases = [0,0,0,0]
 
     def clean_field(self):
         self.field = [0,0,0,0]
+
+def print_playball():
+    print(' _____________________________')
+    print('|                             |')
+    print('|        PLAY BALL!!!!!!!!    |')
+    print('|_____________________________|')
+    time.sleep(2)
+
+def print_scoreboard(local_name, visitor_name, local_points, visitor_points, innings):
+    print(' ___________________________________________________________')
+    print('|             TEAM                |   INNING   |   POINTS   |')
+    print('|---------------------------------|------------|------------|')
+    print('| {} |     {}|{}     |'.format(local_name.ljust(31,' '), innings.ljust(7, ' '),local_points.rjust(7, ' ')))
+    print('| {} |            |{}     |'.format(visitor_name.ljust(31,' '), visitor_points.rjust(7, ' ')))
+    print(' -----------------------------------------------------------')
+    time.sleep(5)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from dependencies import Team, Field
+from dependencies import Team, Field, print_playball, print_scoreboard
 import numpy as np
+import time
 
 class Game:
     """Initialize game"""
@@ -10,16 +11,23 @@ class Game:
         self.inning = 1
         self.local = input('wanna play as local? (Y/N)')
         self.field = Field()
-    
-    #methods
         
     def playball(self):
         """defines Inning and defender/batter control of the game"""
-        while self.inning < 9 or self.team1.score == self.team2.score:
+
+        print_playball()
+
+        while self.inning <= 9 or self.team1.score == self.team2.score:
                 if self.local == 'Y':
+                    print_scoreboard(self.team1.name.title(), self.team2.name.title(), str(self.team1.score), str(self.team2.score), str(self.inning))
                     self.bat(batter=self.team2, defender=self.team1)
+                    print_scoreboard(self.team1.name.title(), self.team2.name.title(), str(self.team1.score), str(self.team2.score), str(self.inning))
+                    self.bat(batter=self.team1, defender=self.team2)
                 else:
+                    print_scoreboard(self.team2.name.title(), self.team1.name.title(), str(self.team2.score), str(self.team1.score), str(self.inning))
                     self.bat(batter=self.team2, defender=self.team1)
+                    print_scoreboard(self.team2.name.title(), self.team1.name.title(), str(self.team2.score), str(self.team1.score), str(self.inning))
+                    self.bat(batter=self.team1, defender=self.team2)
                 self.inning += 1
 
     def bat(self, batter, defender):
@@ -27,13 +35,13 @@ class Game:
         strikes = 0
         outs = 0
         fouls = 0
-        self.field.fill_positions(defender)
 
         while outs < 3:
             while strikes < 3:
+                # "{} is at bat"batter.players[0].name
                 print('pitcher throws')
                 print('batter bats')
-                choice = np.choice([1,2,3]) #1--strike 2--foul 3--hit
+                choice = np.random.choice([1,2,3]) #1--strike 2--foul 3--hit
                 if choice == 1:
                     strikes += 1
                     if strikes == 3 or (strikes + fouls >= 3):
@@ -43,8 +51,8 @@ class Game:
                     fouls += 1
                 elif choice == 3:
                         # define hit techinque (1- first base, 2- second base, 3- third base, 4- HR)
-                        choice = np.choice([1,2,3,4]) # this will be changed once the hit technique is defined
-                        batter.run(self.field.field, choice)
+                        choice = np.random.choice([1,2,3,4]) # this will be changed once the hit technique is defined
+                        batter.run(self.field.bases, choice)
                         strikes = 0
 
 game = Game()
